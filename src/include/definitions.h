c59abaa88b5c202a455bb4905028de56933e5299
nray <nickallenray@gmail.com>
2023-04-14 22:35:51 -0400

    Working skeleton cmake build

---
+++ src/include/definitions.h
@@ -0,0 +1,8 @@
+//
+// Created by nray on 4/14/23.
+//
+
+#ifndef GIUSEP_PI_DEFINITIONS_H
+#define GIUSEP_PI_DEFINITIONS_H
+
+#endif //GIUSEP_PI_DEFINITIONS_H
---
+++ src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by nray on 4/14/23.
+//
+
+
+
+
+
+int main(){
+    return 0;
+};
