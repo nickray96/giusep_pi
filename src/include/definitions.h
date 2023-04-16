6542204db1e94c113bef41dbf0d68ee1d0d5d7b5
nray <nickallenray@gmail.com>
2023-04-16 13:06:06 -0400

    Probably doing something wrong here not tested yet

--- src/include/definitions.h
+++ src/include/definitions.h
@@ -5,4 +5,9 @@
 #ifndef GIUSEP_PI_DEFINITIONS_H
 #define GIUSEP_PI_DEFINITIONS_H
 
+#define SPI_SCK 2
+#define SPI_CS  1
+#define SPI_SO  0
+
+
 #endif //GIUSEP_PI_DEFINITIONS_H
--- src/main.c
+++ src/main.c
@@ -2,10 +2,82 @@
 // Created by nray on 4/14/23.
 //
 
+#include "pico/multicore.h"
+#include "pico/stdlib.h"
+#include "include/definitions.h"
+#include "include/cli.h"
+#include "hardware/spi.h"
+#include <stdio.h>
+
+static const uint8_t REG_DEVID = 0x00;
+
+void reg_read(spi_inst_t *spi, const uint8_t reg, const int16_t *dest){
+
+    uint8_t mb = 0;
+
+    // Construct message (set ~W bit high)
+    uint8_t msg = 0x80 | (mb << 6) | reg;
+
+    // Read from register
+    gpio_put(SPI_CS, 0);
+    spi_write_blocking(spi, &msg, 1);
+    spi_read16_blocking(spi, 0, dest, 1);
+    gpio_put(SPI_CS, 1);
+
+}
 
 
+float get_celsius(const uint16_t *input_data) {
 
+    gpio_put(SPI_CS, 0);
+    sleep_ms(10);
+
+    gpio_put(SPI_CS, 1);
+
+    if (*input_data & 0x4) {
+        // uh oh, no thermocouple attached!
+        return -100;
+        // return -100;
+    }
+
+    uint16_t temp = *input_data >> 3;
+
+    return temp * 0.25;
+}
+
+
+void init_therm(){
+    stdio_init_all();
+
+    gpio_init(SPI_CS);
+    gpio_set_dir(SPI_CS, GPIO_OUT);
+    gpio_put(SPI_CS, 1);
+
+    spi_set_format( spi0,   // SPI instance
+                    8,      // Number of bits per transfer
+                    1,      // Polarity (CPOL)
+                    1,      // Phase (CPHA)
+                    SPI_MSB_FIRST);
+
+    gpio_set_function(SPI_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_SO, GPIO_FUNC_SPI);
+
+
+}
 
 int main(){
+
+    // Start the CLI
+    // multicore_launch_core1(cli_main);
+    spi_inst_t *spi = spi0;
+    uint16_t *data;
+    reg_read(spi, REG_DEVID, data);
+    init_therm();
+    spi_init(spi, 1000 * 1000);
+
+    float temp = get_celsius(data);
+    printf("Celsius: %.4f\n", temp);
+    sleep_ms(1000);
+
     return 0;
 };
