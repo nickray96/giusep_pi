536dfa2182d66137bc3583464dd0e24f675cfbb4
nray <nickallenray@gmail.com>
2023-04-18 23:00:00 -0400

    Drive relay from switch input, remove unused CLI

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
 #include "include/definitions.h"
-#include "include/cli.h"
 #include "hardware/spi.h"
 #include <stdio.h>
 
@@ -23,10 +22,31 @@ short get_celsius(spi_inst_t *spi, uint16_t *input_data) {
     return celcius;
 }
 
+void blink() {
+    static int current_led_state = 0;
 
-void init_therm(spi_inst_t *spi){
+    current_led_state = !current_led_state;
+    gpio_put(HEALTH_LED, current_led_state);
+
+}
+
+void set_boiler_relay(){
+    int boiler_switch_state = gpio_get(BOILER_SWITCH_INPUT);
+
+    // Boiler relay is active low
+    if(boiler_switch_state != 0){
+        gpio_put(BOILER_RELAY_OUTPUT, 0);
+    }
+    else{
+        gpio_put(BOILER_RELAY_OUTPUT, 1);
+    }
+}
+
+
+void init_peripherals(spi_inst_t *spi) {
     stdio_init_all();
 
+    // Init SPI
     gpio_init(SPI_CS);
     gpio_set_dir(SPI_CS, GPIO_OUT);
     gpio_put(SPI_CS, 1);
@@ -36,36 +56,47 @@ void init_therm(spi_inst_t *spi){
     gpio_set_function(SPI_SCK, GPIO_FUNC_SPI);
     gpio_set_function(SPI_SO, GPIO_FUNC_SPI);
 
-    spi_set_format( spi0,      // SPI instance
-                    16,   // Number of bits per transfer
-                    1,       // Polarity (CPOL)
-                    1,      // Phase (CPHA)
-                    SPI_MSB_FIRST);
+    spi_set_format(spi0,      // SPI instance
+                   16,   // Number of bits per transfer
+                   1,       // Polarity (CPOL)
+                   1,      // Phase (CPHA)
+                   SPI_MSB_FIRST);
+
+    //Init blinky LED
+    gpio_init(HEALTH_LED);
+    gpio_set_dir(HEALTH_LED, GPIO_OUT);
+    gpio_put(HEALTH_LED, 0);
+
+    // Init boiler switch input
+    gpio_init(BOILER_SWITCH_INPUT);
+    gpio_set_dir(BOILER_SWITCH_INPUT, GPIO_IN);
+    gpio_pull_down(BOILER_SWITCH_INPUT);
+
+    // Init boiler relay output, secondary not used yet but driving low to keep from floating
+    gpio_init(BOILER_RELAY_OUTPUT);
+    gpio_init(BOILER_SECONDARY_RELAY_OUTPUT);
+    gpio_set_dir(BOILER_RELAY_OUTPUT, GPIO_OUT);
+    gpio_set_dir(BOILER_SECONDARY_RELAY_OUTPUT, GPIO_OUT);
+    gpio_put(BOILER_RELAY_OUTPUT, 1);
+    gpio_put(BOILER_SECONDARY_RELAY_OUTPUT, 1);
 
 }
 
-int main(){
+int main() {
 
     spi_inst_t *spi = spi0;
-    uint16_t data = 0x0;
-    uint16_t *pData = &data;
+    uint16_t thermocouple_data = 0x0;
+    uint16_t *pTCData = &thermocouple_data;
 
-    init_therm(spi);
+    init_peripherals(spi);
 
+    // TODO: needs to do things from an ISR or something better than sleep_ms
+    while (true) {
 
-    // This launches stdio_init_all, don't do it again
-//    multicore_launch_core1(cli_main);  Prob going to gid rid of this and do something more simple
-    gpio_init(HEALTH_LED);
-    gpio_set_dir(HEALTH_LED, GPIO_OUT);
-
-    while(true) {
-
-        float temp = get_celsius(spi, pData);
+        float temp = get_celsius(spi, pTCData);
         printf("Celsius: %.4f\n", temp);
-
-        gpio_put(HEALTH_LED, 1);
-        sleep_ms(1000);
-        gpio_put(HEALTH_LED, 0);
+        blink();
+        set_boiler_relay();
         sleep_ms(1000);
     }
     return 0;
